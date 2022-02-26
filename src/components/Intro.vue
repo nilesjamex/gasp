bb410c3a26545089ca8a75aae9f6a970534618bf
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-26 14:52:54 +0100

    complete css

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,19 @@
-'<template>
-<p>
-      hello
-</p>
+0.7<template>
+<div class="hero">
+    <nav class="hero--nav">
+        <ul class="hero--nav--list">
+            <li>HOME</li>
+            <li>WORKS</li>
+            <li>ABOUT US</li>
+            <li>CONTACT US</li>
+        </ul>
+    </nav>
+    <div class="hero--text">
+        <h1 class="hero--text--main">
+            We help people build their dream homes.
+        </h1>
+    </div>
+</div>
 </template>
 
 <script>
@@ -12,7 +24,30 @@ export default {
 
 <style lang="scss" scope>
 @import '@/Styles/_variables.scss';
-p {
+.hero {
+    padding: 2rem 4rem;
+
+    .hero--nav--list {
+        color: $gray;
+
+        li {
+            margin: 0.7rem 0;
+        }
+    }
+}
+.hero--text--main {
+    font-family: $manrope;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 10rem;
+    line-height: 187px;
+    letter-spacing: 0.02em;
+    text-transform: uppercase;
     color: $white;
+    transition: all 0.5s ease-in-out;
+
+    &:hover {
+        color: $gray;
+    }
 }
 </style>
