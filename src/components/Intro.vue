980ecb8f6fa64cbde8d1f031c0f63e7dd657fe58
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-26 14:20:00 +0100

    intro comp

---
+++ src/components/Intro.vue
@@ -0,0 +1,18 @@
+'<template>
+<p>
+      hello
+</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scope>
+@import '@/Styles/_variables.scss';
+p {
+    color: $white;
+}
+</style>
