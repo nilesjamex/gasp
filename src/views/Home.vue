a8f8c91b53c6eb6daf2329bf5d0ef106a0fefc9d
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-26 14:24:23 +0100

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <Intro />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Intro from '@/components/Intro.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Intro
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
