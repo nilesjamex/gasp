ff2daa010f6b14c9004cc37818bec8a6ba7c7d59
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-26 14:24:44 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+
   </div>
   <router-view/>
 </template>
 
+<script>
+export default {
+  
+}
+</script>
+
 <style lang="scss">
 @import './Styles/_variables.scss';
 @import './Styles/_mixin.scss';
+html, body {
+  background-color: $bg;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $bg;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    @include big-font;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
