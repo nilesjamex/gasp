87c33a0157492738be4d553255190baef46dfbe3
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-28 01:22:33 +0100

    imported new components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
  <Intro />
+ <Projects />
+ <About />
 </template>
 
 <script>
 // @ is an alias to /src
-import Intro from '@/components/Intro.vue'
+import Intro from '@/components/Intro.vue';
+import Projects from '@/components/Projects.vue'
+import About from '@/components/About.vue'
 
 export default {
   name: 'Home',
   components: {
-    Intro
+    Intro,
+    Projects,
+    About
   }
 }
 </script>
