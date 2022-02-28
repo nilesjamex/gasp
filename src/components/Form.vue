76a354bdac834c1e192d88dca9c663c27a372206
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-28 01:19:34 +0100

    new components still unused

---
+++ src/components/Form.vue
@@ -0,0 +1,18 @@
+<template>
+        <div class="projects--text">
+        <h1>
+           GA<span>SP</span>
+        </h1>
+        <img class="projects--image" src="@/assets/Images/form.png" alt="">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/Styles/projects.scss';
+</style>
