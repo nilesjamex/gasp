41265d9b157437b799024abf35d22a25f1dc7dee
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-03-01 21:34:18 +0100

    new routes

---
+++ src/views/Code.vue
@@ -0,0 +1,75 @@
+<template>
+<div>
+  <Nav />
+  <div class="details">
+      <div class="details--intro">
+          <div class="details--intro--text">
+              <h1>
+                  FORM
+              </h1>
+          </div>
+          <div class="details--intro--sub">
+              <h4>
+                  duration: 6 months
+              </h4>
+              <h4>
+                  Location: Ontario, Canada
+              </h4>
+          </div>
+      </div>
+      <div class="details--image">
+          <img src="@/assets/Details/form.png" alt="">
+      </div>
+      <div class="details--projects">
+          <div class="details--projects--description">
+              <h1>
+                  project description
+              </h1>
+          </div>
+          <div class="details--projects--text">
+              <h5>
+                  we were hired to work on a mansion in one of the beautiful estates
+                   in ontario, canada. We brought in our creativity and experience and 
+                  we gave the client a satisfying and good look looking deliveries.
+              </h5>
+          </div>
+          <div class="details--project--image">
+              <img src="@/assets/Details/big-white.png" alt="">
+              <img src="@/assets/Details/small-white.png" alt="">
+              <img src="@/assets/Details/stairs.png" alt="">
+          </div>
+      </div>
+      <div class="details--other--project">
+          <h1>
+              other projects
+          </h1>
+      </div>
+      <Gasp />
+      <Bush />
+      <Both />
+      <Code />
+  </div>
+  </div>
+</template>
+
+<script>
+import Nav from "../components/Nav.vue"
+import Gasp from "../components/Projects/Gasp.vue"
+import Bush from "../components/Projects/Bush.vue"
+import Both from "../components/Projects/Both.vue"
+import Code from "../components/Projects/Code.vue"
+export default {
+components: {
+    Nav,
+    Gasp,
+    Bush,
+    Both,
+    Code
+}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/Styles/details.scss';
+
+</style>
