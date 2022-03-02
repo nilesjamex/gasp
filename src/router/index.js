45a15a3e6fef5667f435b8aff348107ba1e896fd
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-03-02 09:21:36 +0100

    added new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Form from '../views/Form.vue'
+import Gasp from '../views/Gasp.vue'
+import Both from '../views/Both.vue'
+import Bush from '../views/Bush.vue'
+import Code from '../views/Code.vue'
 
 const routes = [
   {
@@ -12,6 +16,26 @@ const routes = [
     path: '/form',
     name: 'Form',
     component: Form
+  },
+  {
+    path: '/gasp',
+    name: 'Gasp',
+    component: Gasp
+  },
+  {
+    path: '/both',
+    name: 'Both',
+    component: Both
+  },
+  {
+    path: '/bush',
+    name: 'Bush',
+    component: Bush
+  },
+  {
+    path: '/code',
+    name: 'Code',
+    component: Code
   }
 ]
 
